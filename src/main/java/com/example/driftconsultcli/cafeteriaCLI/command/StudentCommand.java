f21ed826d6ff9319463b1ef1e4d90347d53a3830
mabunde <solomonsitati39@gmail.com>
2022-06-30 15:30:04 +0300

    custom shell interface

---
+++ src/main/java/com/example/driftconsultcli/cafeteriaCLI/command/StudentCommand.java
@@ -0,0 +1,11 @@
+package com.example.driftconsultcli.cafeteriaCLI.command;
+
+import com.example.driftconsultcli.cafeteriaCLI.shell.ShellHelper;
+import lombok.AllArgsConstructor;
+import org.springframework.shell.standard.ShellComponent;
+
+@ShellComponent
+@AllArgsConstructor
+public class StudentCommand {
+    private final ShellHelper shellHelper;
+}
--- src/main/java/com/example/driftconsultcli/cafeteriaCLI/shell/CliPromptProvider.java
+++ src/main/java/com/example/driftconsultcli/cafeteriaCLI/shell/CliPromptProvider.java
@@ -1,4 +1,4 @@
-package com.example.driftconsultcli.cafeteriaCLI;
+package com.example.driftconsultcli.cafeteriaCLI.shell;
 
 import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStyle;
