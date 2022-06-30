a5879e48821a961a11047d52ecd02ad74af25140
mabunde <solomonsitati39@gmail.com>
2022-06-30 12:02:17 +0300

    initial commit

---
+++ src/main/java/com/example/driftconsultcli/DriftConsultCliApplication.java
@@ -0,0 +1,13 @@
+package com.example.driftconsultcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriftConsultCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriftConsultCliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/driftconsultcli/DriftConsultCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.driftconsultcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriftConsultCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
